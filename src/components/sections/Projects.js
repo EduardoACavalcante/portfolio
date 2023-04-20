ea2320f459cefb54fd9ee508f94c5b278cb66c6b
EduardoACavalcante <eduacavalcante54@gmail.com>
2023-04-20 18:09:45 -0300

    Editadas imagens de projetos

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -12,35 +12,35 @@ class Projects extends Component{
                     <Row style={{borderBottom: "5px solid #0087ca", width: "400px", textAlign: "center", margin: "35px 0 10px", fontSize: "35px", fontWeight: "bold", color: "var(--secondary-color)"}}>Projetos</Row>
                     <Row>
                       <Col md={4}>
-                        <div className="p-0 border" style={{minHeight: "330px"}}>
+                        <div className="p-0 border" style={{height: "225px", overflow: "hidden"}}>
                           <a href="https://minhavez.app" className="text-center">
                             <img src="projects/minhavez.png" style={{width: "100%"}}/>
                           </a>
                         </div>
                       </Col>
                       <Col md={4}>
-                        <div className="p-0 border" style={{minHeight: "330px"}}>
+                        <div className="p-0 border" style={{height: "225px", overflow: "hidden"}}>
                           <a href="https://inline-extractor.vercel.app" className="text-center">
                             <img src="projects/inline-extractor.png" style={{width: "100%"}}/>
                           </a>
                         </div>
                       </Col>
                       <Col md={4}>
-                        <div className="p-0 border" style={{minHeight: "330px"}}>
+                        <div className="p-0 border" style={{height: "225px", overflow: "hidden"}}>
                           <a href="https://chee.com.br" className="text-center">
                             <img src="projects/chega-hora-essa.png" style={{width: "100%"}}/>
                           </a>
                         </div>
                       </Col>
                       <Col md={4}>
-                        <div className="p-0 border" style={{minHeight: "330px"}}>
+                        <div className="p-0 border" style={{height: "225px", overflow: "hidden"}}>
                           <a href="https://eduardocavalcante.vercel.app" className="text-center">
                             <img src="projects/portifolio.png" style={{width: "100%"}}/>
                           </a>
                         </div>
                       </Col>
                       <Col md={4} className="d-none d-md-block">
-                        <div className="p-3 border d-flex justify-content-center align-items-center project-div square" style={{minHeight: "330px", color: "var(--secondary-color)"}}>
+                        <div className="p-3 border d-flex justify-content-center align-items-center project-div square" style={{height: "225px", overflow: "hidden", color: "var(--secondary-color)"}}>
                           <div className="ml-auto mr-auto">
                             <div>Em breve</div>
                             <div style={{marginLeft: "44px", marginTop: "5px"}}>
@@ -50,7 +50,7 @@ class Projects extends Component{
                         </div>
                       </Col>
                       <Col md={4} className="col-md-4 d-none d-md-block">
-                        <div className="p-3 border d-flex justify-content-center align-items-center project-div square" style={{minHeight: "330px", color: "var(--secondary-color)"}}>
+                        <div className="p-3 border d-flex justify-content-center align-items-center project-div square" style={{height: "225px", overflow: "hidden", color: "var(--secondary-color)"}}>
                           <div className="ml-auto mr-auto">
                             <div>Em breve</div>
                             <div style={{marginLeft: "44px", marginTop: "5px"}}>
