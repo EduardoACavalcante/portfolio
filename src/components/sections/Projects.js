54121cc92a1dfa8204663ab4e1ff77751c6bd8e3
EduardoACavalcante <eduacavalcante54@gmail.com>
2023-04-25 02:18:06 -0300

    Added dividers in projects

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -21,6 +21,7 @@ class Projects extends Component {
                       <img src="projects/minhavez.png" style={{ width: "100%" }} />
                     </a>
                   </div>
+                  <hr class="hr hr-blurry d-md-none d-lg-none d-block" />
                 </Col>
                 <Col md={4}>
                   <div className="p-0 border" style={{ height: "225px", overflow: "hidden" }}>
@@ -28,6 +29,7 @@ class Projects extends Component {
                       <img src="projects/inline-extractor.png" style={{ width: "100%" }} />
                     </a>
                   </div>
+                  <hr class="hr hr-blurry d-md-none d-lg-none d-block" />
                 </Col>
                 <Col md={4}>
                   <div className="p-0 border" style={{ height: "225px", overflow: "hidden" }}>
@@ -35,6 +37,7 @@ class Projects extends Component {
                       <img src="projects/chega-hora-essa.png" style={{ width: "100%" }} />
                     </a>
                   </div>
+                  <hr class="hr hr-blurry d-md-none d-lg-none d-block" />
                 </Col>
                 <Col md={4}>
                   <div className="p-0 border" style={{ height: "225px", overflow: "hidden" }}>
@@ -42,6 +45,7 @@ class Projects extends Component {
                       <img src="projects/portifolio.png" style={{ width: "100%" }} />
                     </a>
                   </div>
+                  <hr class="hr hr-blurry d-md-none d-lg-none d-block" />
                 </Col>
                 <Col md={4} className="d-none d-md-block">
                   <div className="p-3 border d-flex justify-content-center align-items-center project-div square" style={{ height: "225px", overflow: "hidden", color: "var(--secondary-color)" }}>
@@ -52,6 +56,7 @@ class Projects extends Component {
                       </div>
                     </div>
                   </div>
+                  <hr class="hr hr-blurry d-md-none d-lg-none d-block" />
                 </Col>
                 <Col md={4} className="col-md-4 d-none d-md-block">
                   <div className="p-3 border d-flex justify-content-center align-items-center project-div square" style={{ height: "225px", overflow: "hidden", color: "var(--secondary-color)" }}>
@@ -62,6 +67,7 @@ class Projects extends Component {
                       </div>
                     </div>
                   </div>
+                  <hr className="hr hr-blurry d-md-none d-lg-none d-block" />
                 </Col>
               </Row>
             </Col>
